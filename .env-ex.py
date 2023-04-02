7f3765c41261b982b4ed77db9e29318d1e186897
philip-persan <phillipeslash@gmail.com>
2023-04-02 17:16:06 -0300

    first commit, initial config's

---
+++ .env-ex.py
@@ -0,0 +1,19 @@
+# * Django Secret Key
+
+SECRET_KEY = 'CHANGE-ME'
+
+# * DEBUG
+DEBUG = 'CHANGE-ME'
+
+# * SQLite
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+DATABASE_NAME = 'db.sqlite3'
+
+# * Postgres (If you use postgres, uncomment the next two lines)
+# * DATABASE_ENGINE='django.db.backends.postgresql'
+# * DATABASE_NAME="braf"
+
+DATABASE_USER = 'CHANGE-ME'
+DATABASE_PASSWORD = 'CHANGE-ME'
+DATABASE_HOST = 'CHANGE-ME'
+DATABASE_PORT = 'CHANGE-ME'
